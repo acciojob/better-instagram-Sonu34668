d357d16bd233cf1572ac8111f437c024e5cfa595
Sonu34668 <130197661+Sonu34668@users.noreply.github.com>
2023-07-19 22:41:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-//your code here
+class Grid {
+  constructor(element) {
+    this.element = element;
+    this.images = Array.from(element.querySelectorAll(".image"));
+    this.init();
+  }
+
+  init() {
+    this.images.forEach(image => {
+      image.addEventListener("dragstart", this.onDragStart);
+      image.addEventListener("drop", this.onDrop);
+    });
+  }
+
+  onDragStart(event) {
+    event.preventDefault();
+    this.draggedImage = event.target;
+    this.draggedImage.classList.add("dragged");
+    this.draggedImage.style.cursor = "move";
+  }
+
+  onDrop(event) {
+    event.preventDefault();
+    this.draggedImage.classList.remove("dragged");
+    this.draggedImage.style.cursor = "default";
+
+    const target = event.target;
+    const index = this.images.indexOf(target);
+    const draggedIndex = this.images.indexOf(this.draggedImage);
+
+    this.images.splice(index, 1);
+    this.images.splice(draggedIndex, 0, this.draggedImage);
+  }
+}
+
+const grid = new Grid(document.querySelector(".grid"));
